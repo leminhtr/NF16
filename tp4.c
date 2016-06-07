abb7db4d8968f1357b8d9bbf33d9c76372343e8a
Minh Tri Lê <minh-tri.le@etu.utc.fr>
2016-06-07 16:33:47 +0200

    case 2 en cours

--- tp4.c
+++ tp4.c
@@ -44,88 +44,115 @@ Dir* search_dir(char* name, Node* sub){
 }
 
 void print_tree(Node* sub){
-    if(sub->lc!=NULL)
-        print_tree(sub->lc);
-    if(sub->dir->status==1)
-        printf("%s\n",sub->dir->name);
-    if(sub->rc!=NULL)
-        print_tree(sub->rc);
+    if(sub!=NULL){
+        if(sub->lc!=NULL)
+            print_tree(sub->lc);
+        if(sub->dir->status==1)
+            printf("%s\n",sub->dir->name);
+        if(sub->rc!=NULL)
+            print_tree(sub->rc);
+    }
+    printf("[end]\n");
 }
 
-int add_dir_to_sub(Dir *dir, Node **sub){
-    if(dir==NULL)   //argument NULL
+Dir *to_father_dir(Dir* dir){
+    if(dir!=NULL){
+        if(dir->father==NULL){
+            printf("Ce repertoire n'a pas de repertoire parent ! Nous restons dans ce repertoire\n");
+            return dir;
+        }
+        return dir->father;
+    }
+    printf("Ce repertoire n'existe pas\n");
+    return NULL;
+}
 
-        return -1;
-
-    if((*sub)->dir==NULL)   //aucun sous-dir
+int add_dir_to_sub(Dir *dir, Node *sub, Dir *father){
+    if(dir==NULL || father==NULL)   //argument NULL
     {
-        //printf("test");
+        printf("Erreur. Le dossier a ajoute ou le dossier pere n'existe pas.\n");
+        return -1;
+    }
+
+    dir->father=father; //Lien avec le dossier p√®re.
+
+    if(sub->dir==NULL)   //aucun sous-dir
+    {
         //Node *new_sub=create_node(dir,NULL, NULL);
-        (*sub)->dir=dir;
+        sub->dir=dir;
+        printf("%s a ete ajoute au dossier %s.\n", dir->name, father->name);
         return 0;
     }
 
-    int comp, comp_left, comp_right;
-    comp=strcmp((*sub)->dir->name, dir->name);
-    comp_left=strcmp((*sub)->lc->dir->name, dir->name);
-    comp_right=strcmp((*sub)->rc->dir->name, dir->name);
+    int comp;
+    comp=strcmp(sub->dir->name, dir->name);
 
     if(comp>0)  //sub>dir
     {
-        if((*sub)->lc==NULL) //fils gauche NULL
+        if(sub->lc==NULL) //fils gauche dispo
         {
-            Node *lc=create_node(dir,NULL, NULL);	//crÈation du fils gauche puis ajout
-            (*sub)->lc=lc;
+            Node *lc=create_node(dir,NULL, NULL);
+            sub->lc=lc;
+            printf("%s a ete ajoute au dossier %s.\n", dir->name, father->name);
             return 0;
         }
-        else    //fils gauche non libre => ajout fils gauche (rÈcursif)
+        else    //fils gauche non libre => ajout fils gauche (r√©cursif)
         {
-            Node **ptr_node_lc=&((*sub)->lc);	//Besoin argument type Node **
-            return add_dir_to_sub(dir, ptr_node_lc);
+            return add_dir_to_sub(dir, sub->lc,father);
         }
     }
 
     if(comp<0)  //sub<dir
     {
-        if((*sub)->rc==NULL) //fils droit NULL
+        if(sub->rc==NULL) //fils droit dispo
         {
-            Node *rc=create_node(dir,NULL,NULL);	//crÈation du fils droit puis ajout
-            (*sub)->rc=rc;
+            Node *rc=create_node(dir,NULL,NULL);
+            sub->rc=rc;
+            printf("%s a ete ajoute au dossier %s.\n", dir->name, father->name);
             return 0;
         }
-        else    //fils droit non libre => ajout fils droit (rÈcursif)
+        else    //fils droit non libre => ajout fils droit (rÔøΩcursif)
         {
-            Node **ptr_node_rc=&((*sub)->rc); 	//Besoin argument type Node **
-            return add_dir_to_sub(dir, ptr_node_rc);
+            return add_dir_to_sub(dir, sub->rc, father);
         }
 
     }
 
-    if(comp==0 || comp_left==0 || comp_right==0)    //dir existe dÈj‡
+    //comp_left=strcmp(sub->lc->dir->name, dir->name);
+    //comp_right=strcmp(sub->rc->dir->name, dir->name);
+    if(comp==0)    //dir existe d√©j√†
+    {
+        printf("Ce dossier existe deja.\n");
         return -1;
+    }
+
 
     return 0;
 }
 
-void print_path(Dir *dir)
+int print_path(Dir *dir)
 {
-    int i=0; 
+    if(dir==NULL)
+        return -1;
+    int i=0;
     char *tab_absolute_dir_name[DMAX];
     Dir *temp_father=malloc(sizeof(Dir));	//temp directory
     temp_father=dir;
 
-    while(temp_father!=NULL)    //stockage nom dossier de current ‡ racine
+    while(temp_father!=NULL)    //stockage nom dossier de current  racine
     {
         tab_absolute_dir_name[i]=temp_father->name;
         temp_father=temp_father->father;
         i++;
     }
-    
+
     i--;    //Annule dernier i++ en trop du while
-    while(i!=0)     //affichage nom dossier de racine ‡ current (donc ordre dÈcroissant du tableau)
+    printf("Arborescence : ");
+    while(i>=0)     //affichage nom dossier de racine  current (donc ordre d√©croissant du tableau)
     {
         printf("%s/", tab_absolute_dir_name[i]);
         i--;
     }
-
+    printf("\n");
+    return 0;
 }
